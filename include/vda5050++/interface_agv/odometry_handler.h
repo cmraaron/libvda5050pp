82225db82ae86cdfe3bf9db10a3bffe116ea79f5
Jonas Röger <jonas.kieran.roeger@iml.fraunhofer.de>
2022-05-03 17:00:44 +0200

    add interface_agv::OdometryHandler

---
+++ include/vda5050++/interface_agv/odometry_handler.h
@@ -0,0 +1,89 @@
+//  Copyright 2022 Open Logistics Foundation
+//
+//  Licensed under the Open Logistics License 1.0.
+//  For details on the licensing terms, see the LICENSE file.
+
+#ifndef INCLUDE_VDA5050_2B_2B_INTERFACE_AGV_ODOMETRY_HANDLER_H_
+#define INCLUDE_VDA5050_2B_2B_INTERFACE_AGV_ODOMETRY_HANDLER_H_
+
+#include <chrono>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <thread>
+
+#include "vda5050++/core/common/interruptable_timer.h"
+#include "vda5050++/model/AGVPosition.h"
+#include "vda5050++/model/Velocity.h"
+
+namespace vda5050pp::interface_agv {
+// Forward declaration
+class Handle;
+
+class OdometryHandler {
+  friend class Handle;
+
+private:
+  ///
+  ///\brief Pointer to the current library handle instance
+  ///
+  /// This cannot be a std::optional<std::reference_wrapper<Handle>> due to
+  /// the lack of incomplete type support
+  ///
+  Handle *handle_ptr_ = nullptr;
+
+  void setHandleRef(Handle &handle) noexcept(true);
+
+  std::unique_ptr<std::thread> visualization_thread_;
+
+  vda5050pp::core::common::InterruptableTimer visualization_timer_;
+
+public:
+  class NotAttachedError : public std::logic_error {
+  public:
+    NotAttachedError() noexcept(true);
+  };
+
+  virtual ~OdometryHandler();
+
+  ///
+  ///\brief This function is called, when a initializePosition
+  /// instant actions was received
+  ///
+  ///\param pos the AGVPosition told by the MC
+  ///
+  virtual void initializePosition(const vda5050pp::AGVPosition &pos) = 0;
+
+  ///
+  ///\brief Set the current AGVPosition (only for visualization purposes)
+  ///
+  /// NOTE: This does not evaluate the position. The NavigationHandler still
+  /// has to process the position separately:
+  ///
+  ///\param pos the current AGVPosition
+  ///
+  void setAGVPosition(const vda5050pp::AGVPosition &pos) noexcept(false);
+
+  ///
+  ///\brief Set the current Velocity
+  ///
+  ///\param vel the current Velocity
+  ///
+  void setVelocity(const vda5050pp::Velocity &vel) noexcept(false);
+
+  ///
+  ///\brief Starts a thread, which periodically sends Visualization Messages
+  ///
+  ///\param period the message rate period
+  ///
+  void enableAutomaticVelocityMessages(std::chrono::system_clock::duration period) noexcept(false);
+
+  ///
+  ///\brief If the visualization thread is running, stop it.
+  ///
+  void disableAutomaticVelocityMessages() noexcept(true);
+};
+
+}  // namespace vda5050pp::interface_agv
+
+#endif  // INCLUDE_VDA5050_2B_2B_INTERFACE_AGV_ODOMETRY_HANDLER_H_
---
+++ src/vda5050++/interface_agv/odometry_handler.cpp
@@ -0,0 +1,74 @@
+#include "vda5050++/interface_agv/odometry_handler.h"
+
+#include "vda5050++/core/interface_agv/handle_accessor.h"
+#include "vda5050++/interface_agv/status/visualization.h"
+
+using namespace vda5050pp::interface_agv;
+
+void OdometryHandler::setHandleRef(Handle &handle) noexcept(true) { this->handle_ptr_ = &handle; }
+
+OdometryHandler::NotAttachedError::NotAttachedError() noexcept(true)
+    : std::logic_error("OdometryHandler was not attached to the library handle") {}
+
+void OdometryHandler::setAGVPosition(const vda5050pp::AGVPosition &pos) noexcept(false) {
+  if (this->handle_ptr_ == nullptr) {
+    throw NotAttachedError();
+  }
+
+  vda5050pp::core::interface_agv::HandleAccessor ha(*this->handle_ptr_);
+
+  ha.getState().setAGVPosition(pos);
+}
+
+OdometryHandler::~OdometryHandler() { this->disableAutomaticVelocityMessages(); }
+
+void OdometryHandler::setVelocity(const vda5050pp::Velocity &vel) noexcept(false) {
+  if (this->handle_ptr_ == nullptr) {
+    throw NotAttachedError();
+  }
+
+  vda5050pp::core::interface_agv::HandleAccessor ha(*this->handle_ptr_);
+
+  ha.getState().setVelocity(vel);
+}
+
+void OdometryHandler::enableAutomaticVelocityMessages(
+    std::chrono::system_clock::duration period) noexcept(false) {
+  if (this->handle_ptr_ == nullptr) {
+    throw NotAttachedError();
+  }
+
+  this->disableAutomaticVelocityMessages();
+
+  auto vis_task = [this, period] {
+    auto running = true;
+    auto wakeup_time = std::chrono::system_clock::now() + period;
+    auto wakeup_time_2 = wakeup_time + period;
+
+    while (running) {
+      switch (this->visualization_timer_.sleepUntil(wakeup_time)) {
+        case core::common::InterruptableTimerStatus::k_ok:
+          vda5050pp::interface_agv::status::sendVisualization(*this->handle_ptr_);
+          wakeup_time = wakeup_time_2;
+          wakeup_time_2 = wakeup_time + period;
+          break;
+        case core::common::InterruptableTimerStatus::k_interrupted:
+          // Do nothing and sleep again next iteration
+          break;
+        default:
+        case core::common::InterruptableTimerStatus::k_disabled:
+          running = false;
+          break;
+      }
+    }
+  };
+
+  this->visualization_timer_.enable();
+  this->visualization_thread_ = std::make_unique<std::thread>(vis_task);
+}
+void OdometryHandler::disableAutomaticVelocityMessages() noexcept(true) {
+  this->visualization_timer_.disable();
+  if (this->visualization_thread_ != nullptr) {
+    this->visualization_thread_->join();
+  }
+}
